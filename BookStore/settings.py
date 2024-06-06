bb2c92a77cf10c3d81221aa5644f6db743b6d668
7roubb <7roubb@gmail.com>
2024-06-06 18:24:21 +0300

    Models

--- BookStore/settings.py
+++ BookStore/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/image'
 import os 
 STATICFILES_DIR=[
     os.path.join(BASE_DIR,'static')
--- Store/models.py
+++ Store/models.py
@@ -1,3 +1,76 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Tag (models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=100,null=True, blank=True)
+    author = models.CharField(max_length=100 ,null=True, blank=True)
+    price = models.CharField(max_length=100 ,null=True)
+    issuDate = models.DateField(null=True, blank=True)
+    CATOGRTY = [
+        ('Fiction', 'Fiction'),
+        ('Non-Fiction', 'Non-Fiction'),
+        ('Biography', 'Biography'),
+        ('Thriller', 'Thriller'),
+        ('Romance', 'Romance'),
+        ('Horror', 'Horror'),
+        ('Mystery', 'Mystery'),
+        ('Sci-Fi', 'Sci-Fi'),
+        ('Self-Help', 'Self-Help'),
+        ('Health', 'Health'),
+        ('Cooking', 'Cooking'),
+        ('History', 'History'),
+        ('Travel', 'Travel'),
+        ('Education', 'Education'),
+        ('Business', 'Business'),
+        ('Politics', 'Politics'),
+        ('Religion', 'Religion'),
+        ('Spirituality', 'Spirituality'),
+        ('Science', 'Science'),
+        ('Technology', 'Technology'),
+
+    ]
+    category = models.CharField(max_length=100, choices=CATOGRTY)
+    tag = models.ManyToManyField(Tag, blank=True, null= True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    STATUS = [
+       ('IN ORDER','IN ORDER'),
+        ('DELIVERED','DELIVERED'),
+        ('CANCELLED','CANCELLED'),
+        ('RETURNED','RETURNED'),
+        ('PENDING','PENDING'),
+        ('DELIVERING','DELIVERING'),
+
+    ]
+    status = models.CharField(max_length=100, choices=STATUS)
+
+    def __str__(self):
+        return self.customer.name
